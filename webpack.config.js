55a0a1dbeb9cb21cdf513a4bcace5f6571781886
Denis <example@gmail.com>
2023-05-01 00:45:29 +0700

    push

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,11 @@
 let path = require('path');
 
 module.exports = {
-  mode: 'development', // режим в котором будет работать webpack.
-  entry: './js/script.js', //Точка входа указывает, какой модуль webpack должен использовать
+  mode: 'development',
+  entry: './js/script.js',
   output: {
-    filename: 'bundle.js', // название нового файла
-    path: __dirname + '/js' // куда будем складывать. dir - корневая папка
+    filename: 'bundle.js',
+    path: __dirname + '/js'
   },
   watch: true,
 
