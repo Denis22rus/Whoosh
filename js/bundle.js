464168af57d8109e4fe1ce16433b7e0bcf1543dd
Denis <example@gmail.com>
2023-05-03 01:53:32 +0700

    blue-fonts

--- js/bundle.js
+++ js/bundle.js
@@ -15,11 +15,13 @@ __webpack_require__.r(__webpack_exports__);
 function burger() {
     const iconMenu = document.querySelector('.icon-menu');
     const menuBody = document.querySelector('.header__menu');
+    const blueFonts = document.querySelector('.blue-fonts');
     if (iconMenu) {
         iconMenu.addEventListener("click", function (e) {
             document.body.classList.toggle('_lock');
             iconMenu.classList.toggle('_active');
             menuBody.classList.toggle('_active');
+            blueFonts.style.display = blueFonts.style.display === 'block' ? 'none' : 'block';
         });
     }
   }
--- js/modules/burger.js
+++ js/modules/burger.js
@@ -1,11 +1,13 @@
 export default function burger() {
     const iconMenu = document.querySelector('.icon-menu');
     const menuBody = document.querySelector('.header__menu');
+    const blueFonts = document.querySelector('.blue-fonts');
     if (iconMenu) {
         iconMenu.addEventListener("click", function (e) {
             document.body.classList.toggle('_lock');
             iconMenu.classList.toggle('_active');
             menuBody.classList.toggle('_active');
+            blueFonts.style.display = blueFonts.style.display === 'block' ? 'none' : 'block';
         });
     }
   }
